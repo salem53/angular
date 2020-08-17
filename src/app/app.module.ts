7ce53ac3ee20c2364138e91220c074585387b413
salemm53 <salem.dhouimir@ensi-uma.tn>
2020-08-17 01:47:27 +0100

    angular components part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,29 @@ import { FlexLayoutModule} from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 
-import 'hammerjs' ;
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component' ;
+import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
